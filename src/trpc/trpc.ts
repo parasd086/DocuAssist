7a70a54cf770137cf6aab4adfa64cca3ffc15ffe
parasd086 <parasd086@gmail.com>
2024-07-05 21:37:33 +0530

    Add file schema and procedure

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,7 +1,27 @@
-import { initTRPC } from "@trpc/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { TRPCError, initTRPC } from "@trpc/server";
 
 const t = initTRPC.create();
+const middleware = t.middleware;
+
+const isAuth = middleware(async (opts) => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || !user.id) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return opts.next({
+    ctx: {
+      userId: user.id,
+      user,
+    },
+  });
+});
 
 export const router = t.router;
 //publicProcedure allows us to create API endpoint regardless of authentication
 export const publicProcedure = t.procedure;
+//Whenever somebdoy call privateProc.. make sure to run isAuth beforehand
+export const privateProcedure = t.procedure.use(isAuth);
