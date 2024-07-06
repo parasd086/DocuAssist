2cbc5296f0062e60ad1d6e469977760a0ab4cd48
parasd086 <parasd086@gmail.com>
2024-07-06 12:32:30 +0530

    Add upload pdf button mockup

---
+++ src/components/ChatWrapper.tsx
@@ -0,0 +1,5 @@
+const ChatWrapper = () => {
+  return <div>chatwrapper</div>;
+};
+
+export default ChatWrapper;
---
+++ src/components/PdfRenderer.tsx
@@ -0,0 +1,5 @@
+const PdfRenderer = () => {
+  return <div>pdfrenderen</div>;
+};
+
+export default PdfRenderer;
