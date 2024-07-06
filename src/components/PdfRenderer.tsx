fb9920270d05b6f6018795812e54ad616cb858e4
parasd086 <parasd086@gmail.com>
2024-07-06 17:21:45 +0530

    Add pdf render

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,5 +1,69 @@
-const PdfRenderer = () => {
-  return <div>pdfrenderen</div>;
+"use client";
+
+import {
+  ChevronDown,
+  ChevronUp,
+  Loader2,
+  RotateCw,
+  Search,
+} from "lucide-react";
+import { Document, Page, pdfjs } from "react-pdf";
+
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+import { useToast } from "./ui/use-toast";
+import { useResizeDetector } from "react-resize-detector";
+
+import { Button } from "./ui/button";
+import { useState } from "react";
+
+import { z } from "zod";
+
+import { cn } from "@/lib/utils";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
+
+interface PdfRendererProps {
+  url: string;
+}
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+  const { toast } = useToast();
+  const { width, ref } = useResizeDetector();
+
+  return (
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5">top bar</div>
+      </div>
+
+      <div className="flex-1 w-full max-h-screen">
+        <div>
+          <Document
+            loading={
+              <div className="flex justify-center">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />
+              </div>
+            }
+            onLoadError={() => {
+              toast({
+                title: "Error loading PDF",
+                description: "Please try again later",
+                variant: "destructive",
+              });
+            }}
+            file={url}
+            className="max-h-full"
+          >
+            <Page pageNumber={1} />
+          </Document>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PdfRenderer;
